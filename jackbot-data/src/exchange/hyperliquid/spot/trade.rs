5943c844853d04caac186e43f332a309f0e38b20
pmatheus <pmatheus.nsx@gmail.com>
2025-05-24 16:19:38 -0300

    Add trade stream aliases

--- jackbot-data/src/exchange/hyperliquid/spot/trade.rs
+++ jackbot-data/src/exchange/hyperliquid/spot/trade.rs
@@ -1,3 +1,23 @@
 //! Trade event types for Hyperliquid Spot.
+//!
+//! Provides convenient aliases for [`Hyperliquid`](super::super::super::Hyperliquid)
+//! trade streams.
 
-pub use super::super::trade::*;
+use crate::{
+    transformer::stateless::StatelessTransformer,
+    subscription::trade::PublicTrades,
+    ExchangeWsStream,
+};
+use super::super::super::Hyperliquid;
+
+pub use super::super::trade::HyperliquidTrades;
+
+/// [`ExchangeTransformer`](crate::transformer::ExchangeTransformer) used to
+/// convert Hyperliquid WebSocket trade messages into [`PublicTrade`](PublicTrades)
+/// events.
+pub type HyperliquidSpotTradesTransformer<InstrumentKey> =
+    StatelessTransformer<Hyperliquid, InstrumentKey, PublicTrades, HyperliquidTrades>;
+
+/// Type alias for a Hyperliquid Spot trades WebSocket stream.
+pub type HyperliquidSpotTradesStream<InstrumentKey> =
+    ExchangeWsStream<HyperliquidSpotTradesTransformer<InstrumentKey>>;
